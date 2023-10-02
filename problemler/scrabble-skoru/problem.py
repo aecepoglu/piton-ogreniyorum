bcdf286325f4cf13871dfb669101857a9102fd18
aecepoglu <aecepoglu@fastmail.fm>
2023-10-02 21:53:18 +0100

    scrabble-skoru problemi eklendi

---
+++ problemler/scrabble-skoru/problem.py
@@ -0,0 +1,30 @@
+# Problem 0
+# verilen bir metindeki harflerin uzerinden gidip,
+#  "e" harfi ise "EVET",
+#            degil ise "HAYIR" yazmak
+# Ornegin "devseu" metni icin su cikti olacak:
+# HAYIR
+# EVET
+# HAYIR
+# HAYIR
+# EVET
+# HAYIR
+
+# Problem 1
+# verilen bir metinde "e" harfinin kac kere gectigini bulmak
+# Ornegin "devseu" metninde "e" harfi 2 kere geciyor
+
+# Problem 2
+# verilen bir metindeki tum harflerin kacar kere gectigini bulmak
+# Ornegin "devseu" metninde:
+#         1 tane d,
+#         2 tane e,
+#         1 tane v,
+#         1 tane s,
+#         1 tane u harfi var
+
+# Problem 3
+# bir kelimenin scrabble skorunu bulmak
+
+# Problem 4
+# elimizdeki bir liste scrabble tasi ile yazilabilecek scrabble puani en yuksek kelimeyi (veya kelimeleri) yazdirmak
