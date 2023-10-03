0d9627be87df9022168a37e707ead644422ad815
aecepoglu <aecepoglu@fastmail.fm>
2023-10-03 11:02:40 +0100

    liste-islemleri ve sayi-okuma problemleri eklendi

---
+++ problemler/liste-islemleri/problem.py
@@ -0,0 +1,15 @@
+# problem
+# bir listeyi istedigimiz bir konumdan ikiye bol
+# Ornegin: [1, 7, 5, 4, 5, 3, 2, 0] listesini 2'nci indexten bolersek [1, 7] ve [5, 4, 5, 3, 2, 0] parcalari olusur
+
+# problem
+# bir listedeki en buyuk elemani bul
+# Ornegin: [5, 4, 5, 3, 2, 0] listesinin en buyuk elemani: 5
+
+# problem
+# bir listede, solundaki tum elemanlardan buyuk olan elemanlari bul
+# Ornegin: [1, 7, 5, 4, 5, 3, 2, 0] listesinde 1 solundaki her seyden (hicbir sey yok) buyuk. 7 de solundaki her seyden daha buyuk.
+
+# problem
+# bir listede, solundaki elemanlarin toplamindan kucuk, ama solundaki elemanlarin her birisinden daha buyuk olan sayilari bul
+# Ornegin: [1, 3, 4, 5, 27, 30] -> [5, 30]
---
+++ problemler/sayi-okuma/problem.py
@@ -0,0 +1,23 @@
+# problem
+# 0-10 arasinda herhangi bir sayinin turkce okunusunu yaz
+# Ornegin: 7 -> yedi
+
+# problem
+# 0-100 araliginda herhangi bir sayinin turkce okunusunu yaz
+# Ornegin: 73 -> yetmis uc
+
+# problem
+# 0-1000 araliginda herhangi bir sayinin turkce okunusunu yaz
+# Ornegin: 973 -> dokuz yuz yetmis uc
+
+# problem
+# 1000000000000'a kadar herhangi bir sayinin turkce okunusunu yaz
+# Ornegin: 903675124 -> dokuz yuz uc milyon alti yuz yetmis bes bin yuz yirmi dort
+
+# problem - bonus 1
+# yaziyla verilmis basit (tek bir islem iceren) aritmetik islemi yap
+# Ornegin: "yuz yetmis uc eksi doksan" -> "seksen uc"
+
+# problem - zor bonus 2
+# verilen okunusun olasi tum sayi karsiliklarini yaz
+# Ornegin: "dokuz yuz yetmis uc" -> [[973], [9, 100, 70, 3], [9, 173], [9, 170, 3]]
