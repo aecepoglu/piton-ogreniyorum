190b5ec79ff8b9a89692376dbd83b8594f0440cc
aecepoglu <aecepoglu@fastmail.fm>
2023-10-06 12:05:54 +0100

    2 yeni problem: meksika dalgasi, bulusma gunleri

---
+++ problemler/kutuphaneler/standart-kutuphane/tarihler/problem.py
@@ -0,0 +1,26 @@
+# Aksekililer Bulusmasi
+# Facebook'taki Aksekililer toplulugunun sayfasinda soyle yaziyor:
+# GRUBUMUZ 2008 YILINDAN BU YANA ANTALYA ATİK'TE HER AYIN İLK CUMARTESİ GÜNÜ SAAT 12.00 DE TOPLANTILAR DÜZENLEMEKTEDİR.
+
+# Bu problemlerde python'daki tarih kutuphanelerini kullanabilirsiniz.
+
+# Problem 1
+# Herhangi bir tarihin haftanin hangi gunu oldugunu bulmak
+# Ornegin: 2000-01-01 haftanin hangi gunuydu?
+# Ornegin: 2008-01-01 haftanin hangi gunuydu?
+
+# Problem 2
+# 2008 Ocak ayinin ilk Cumartesi hangi tarihtedir?
+# 2008 Ocak ayinin ilk gununun haftanin hangi gunu oldugunu zaten bilebiliyoruz.
+
+# Problem 3
+# 2008 yilinin her bir gununun haftanin hangi gunune denk geldigini bul.
+
+# Problem 4
+# 2008 yilinin her bir gununun hangi aya girdigini bul
+
+# Problem 5
+# 2008 yilindaki toplanti tarihlerini bul
+
+# Problem 6
+# 2008 yilindan bu gune tum toplanti tarihlerini bul
---
+++ problemler/terminal-arayuzleri/meksika-dalgasi/problem.py
@@ -0,0 +1,54 @@
+# Problem
+# Soldan saga meksika dalgasi yapan bir tribun yapin
+# Oturan kisiler icin _, ayaktaki kisiler icin ^, ikisinin arasindaki kisiler icin de - karakterini kullanin.
+# Ornegin 3 satir, 5 sutunluk bir tribun otururken bu sekilde gorunur:
+# _____
+# _____
+# _____
+#
+# Dalga bu sekilde baslar, ilerler, ve biter:
+# -____        ^-___        -^-__         _-^-_        __-^-        ___-^        ____-        _____
+# -____   ->   ^-___   ->   -^-__   ->    _-^-_   ->   __-^-   ->   ___-^   ->   ____-   ->   _____
+# -____        ^-___        -^-__         _-^-_        __-^-        ___-^        ____-        _____
+
+# Problem 1
+# tribunde tek bir satir ve 7 koltuk oldugunu dusunelim.
+# Herkesin oturdugu durumu yazdirin: _______
+
+# Problem 2
+# tek satir, 7 sutunluk bir tribunde, koltuk numarasi verilen bir kisiyi yari-ayakta veya tamamen ayakta gosterebilin.
+# Ornegin 5'nci kisi yari-ayakta iken   : ____-__
+#         3'ncu kisi tamamen ayakta iken: __^____
+
+# Problem 3
+# tek satir, 7 sutunluk bir tribunun meksika dalgasini satir satir yazdirin
+# 5 sutunluk bir sutun olsaydi cikti boyle olacakti:
+# _____
+# -____
+# ^-___
+# -^-__
+# _-^-_
+# __-^-
+# ___-^
+# ____-
+# _____
+
+# Problem 4
+# 5 FPS hiz ile tek satir, 7 sutunluk bir tribunun meksika dalgasini yazdirin.
+# print() fonksiyonunun `end=''` argumanini atarsaniz print'ten sonra yeni satira gecmez.
+# Bir "\r" print ederseniz de imlecinizi satir basina goturur. Bu sayede ayni satirda animasyon oynatabilirsiniz
+# 5 FPS hizda animasyon oynatmak icin iki kare arasinda 0.2 saniye beklemelisiniz. `time` kutuphanesindeki `sleep` fonksiyonunu kullanin.
+
+# Problem 5
+# Verilen satir sayisi, sutun sayisi, animasyon hizi (FPS) ve dalga boyu ile bir meksika dalgasi animasyonu oynatin.
+# Dalga boyu: ayaga kalkan iki kisi arasindaki koltuk sayisidir.
+
+# Problem 6 - bonus
+# Farkli satirdaki kisiler arasinda rastgele bir zaman sapmasi olsun. Ornegin 3'ncu satirdaki kisi 1'nci satirdaki kisiden 2 kare sonra meksika dalgasina baslayabilir
+
+# Problem 7 - bonus
+# Her bir koltuktaki kisiye farkli ve rastgele bir renk atayin.
+# Renk yazdirmak icin wikipedia'dan Ansi Escape Sequences konusunu okuyun.
+
+# Problem 8 - bonus
+# Kisiler ayaga kalktikca, renklerini degistirin.
